2000aa1c942263b3744693274cfb8395490d8cc1
David Lindahl <davidalindahl@gmail.com>
2017-12-05 14:27:18 -0800

    setup server file with framework from class

---
+++ lab-david/lib/server.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const http = require('http');
+const winston = require('winston');
+const requestParser = require('./request-parser');
+const faker = require('faker');
+
+
+const winstonLevels = {error: 0, warn : 1, info : 2, verbose : 3, debug : 4};
+
+const logger = new (winston.Logger)({
+  transports : [
+    new (winston.transports.File)({
+      filename : 'log.json',
+      levels : winstonLevels,            
+    }),
+  ],
+});
+
+//---------------------- creating the server -------------------------------------
+const app = http.createServer((request, response) => {
+  logger.log('info', 'Processing Request');
+  logger.log('info', `Method: ${request.method}`);
+  logger.log('info', `URL: ${request.url}`);
+  logger.log('info', `HEADERS: ${JSON.stringify(request.headers)}`);
+
+  //--------------------------------------------------------
+  // PROCESS REQUEST
+  //--------------------------------------------------------
+
+  requestParser.parse(request)
+    .then(request => {
+      if(request.method === 'GET' && request.url.pathname === '/') {
+        response.writeHead(200,{'Content-Type' : 'text/html' });
+
+        response.write(`<html>here</html>`);
+        logger.log('info', 'Responding with a 200 status code');
+        response.end();
+        return;
+      }else if(request.method === 'POST' && request.url.pathname === '/echo'){
+        response.writeHead(200,{'Content-Type' : 'application/json'});
+        response.write(JSON.stringify(request.body));
+        response.end();
+        return;
+      }
+      response.writeHead(404,{ 'Content-Type' : 'text/plain' });
+      response.write('Not Found');
+      logger.log('info', 'Responding with a 404 status code');
+      response.end();
+      return;
+    }).catch(error => {
+      logger.log('info', 'Answering with a 400 status code');
+      logger.log('info',error);
+
+      response.writeHead(400,{ 'Content-Head' : 'text/plain' });
+      response.write('Bad Request');
+      response.end();
+      return;
+    });
+});
+
+const server = module.exports = {};
+
+server.start = (port,callback) => {
+  logger.log('info', `server is up on port ${port}`);
+  return app.listen(port,callback);
+};
+
+server.stop = (callback) => {
+  logger.log('info', `server stopped with no errors`);
+  return app.close(callback);
+};
