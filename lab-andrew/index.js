6e14a8651f592b2351f120467d33d0b204c5a426
Andrew Bloom <blooma116@gmail.com>
2017-12-05 15:59:47 -0800

    cowsay added

--- lab-andrew/index.js
+++ lab-andrew/index.js
@@ -4,4 +4,6 @@ require('dotenv').config();
 const server = require('./lib/server');
 const PORT = process.env.PORT;
 
-server.start(PORT, () => {});
+server.start(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
