c824780fc9515edfdc3d757744b89d0b82c50569
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2017-12-05 21:47:51 -0800

    get cow to say what I tell it to

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -8,18 +8,18 @@ describe(`server.js`, () => {
     return superagent.get(`http://localhost:3000/`)
       .then(response => {
         expect(response.status).toEqual(200);
-      })
-  })
+      });
+  });
   test(`GET should respond with a 200 status when /cowsay is hit with a string provided in the request query`, () => {
     return superagent.get(`http://localhost:3000/cowsay?text=word`)
       .then(response => {
         expect(response.status).toEqual(200);
-      })
-  })
+      });
+  });
   test(`GET should respond with a 200 status when /cowsay is hit with a string provided in the request query`, () => {
     return superagent.get(`http://localhost:3000/cowsay`)
       .then(response => {
         expect(response.status).toEqual(400);
-      })
-  })
-})
+      });
+  });
+});
--- index.js
+++ index.js
@@ -2,12 +2,11 @@
 
 const server = require(`./lib/server`);
 const winston = require(`winston`);
-const cowsay = require(`cowsay`);
 
 let logger = new (winston.Logger)({
   transports: [
     new (winston.transports.File)({ filename: 'log.json' }),
-  ]
+  ],
 });
 
 require(`dotenv`).config(); //this sets up the process.env object with all the variables in .env file
