58e37d2254f6b6111856e82f002728664e79c685
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2017-12-05 22:19:00 -0800

    writing tests to see if post request works when no errors are present

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -16,10 +16,36 @@ describe(`server.js`, () => {
         expect(response.status).toEqual(200);
       });
   });
-  test(`GET should respond with a 200 status when /cowsay is hit with a string provided in the request query`, () => {
+  test(`GET should respond with a 200 status when /cowsay is hit without a string provided in the request query`, () => {
     return superagent.get(`http://localhost:3000/cowsay`)
       .then(response => {
-        expect(response.status).toEqual(400);
+        expect(response.status).toEqual(200);
       });
   });
+  test(`POST should return a 200 status when /api/cowsay is hit and no errors are present`, () => {
+    let bodyToTest = {text : 'moo goes the cow'};
+    return superagent.post(`http://localhost:3000/api/cowsay`)
+    .send(bodyToTest);
+    .then(response =>
+      expect(reponse.status).toEqual(200);
+    })
+  })
+  // test(`POST should respond with a 400 status when /api/cowsay is hit without a body provided`, () => {
+  //   return superagent.post(`http://localhost:3000/api/cowsay`)
+  //   .set({'Content-Type' : 'application/json'})
+  //   .send('}')
+  //   .then(response => Promise.reject(response))
+  //   .catch((response) => {
+  //     expect(reponse.status).toEqual(400);
+  //   })
+  // })
+  // test(`POST should respond with a 400 status when /api/cowsay is hit without text provided`, () => {
+  //   return superagent.post(`http://localhost:3000/api/cowsay`)
+  //   .set({'Content-Type' : 'application/json'})
+  //   .send('}')
+  //   .then(response => Promise.reject(response))
+  //   .catch((response) => {
+  //     expect(reponse.status).toEqual(400);
+  //   })
+  // })
 });
