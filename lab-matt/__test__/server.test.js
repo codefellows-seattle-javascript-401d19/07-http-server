c7651495b8f68a34b7a9e0edfeca4c5dd750a6d8
snobeard <fatmattmatt@gmail.com>
2017-12-05 21:36:50 -0800

    basic GET route workign

--- lab-matt/__test__/server.test.js
+++ lab-matt/__test__/server.test.js
@@ -4,13 +4,13 @@ const superagent = require('superagent');
 
 
 describe('http.js', () => {
-  // test('GET request should respond with a 200 status code and a body if there is no error', () => {
-  //   return superagent.get('http://localhost:3000/')
-  //     .then(response => {
-  //       expect(response.status).toEqual(200);        
-  //       expect(response.body).toEqual('body');
-  //     });
-  // });
+  test('GET request should respond with a 200 status code and a body if there is no error', () => {
+    return superagent.get('http://localhost:3000/')
+      .then(response => {
+        expect(response.status).toEqual(200);        
+        expect(response.text).not.toEqual('body');
+      });
+  });
 
   test('POST request should respond with a 200 status code and a body if there is no error', () => {
     let bodyToTest = {'poodle': 'lola'};
@@ -23,7 +23,6 @@ describe('http.js', () => {
   });
 
   test('POST should respond with a 400 if there is any error', () => {
-    let bodyToTest = {'poodle': 'lola'};
     return superagent.post('http://localhost:3000/echo')
       .set( {'Content-Type': 'application/json'} )
       .send('{')
