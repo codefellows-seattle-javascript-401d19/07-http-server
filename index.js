0b6534d1b1806b1fee0dacfc2f14551143477461
Anthony Robinson <robinsonamk@gmail.com>
2017-12-05 17:59:58 -0800

    get requests are working with cowsay

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+require('dotenv').config();
+
+const server = require('./lib/server');
+
+server.start(process.env.PORT, () => {
+  console.log(`server is running on port ${process.env.PORT}`);
+});
