fedbb2c39f45cdd33970e039daceb2d95dfc2aa3
Dalton Carr <daltonlcarr@yahoo.com>
2017-12-11 16:42:14 -0800

    indexjs

---
+++ index.js
@@ -0,0 +1,10 @@
+
+'use strict';
+
+require('dotenv').config();
+
+const server = require('./lib/server');
+
+server.start(process.env.PORT,() => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
